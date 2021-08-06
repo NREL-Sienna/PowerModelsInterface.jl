3aa97b477b9588cdf078e3afc4f6af7752c143ac
Jose Daniel Lara <jdlara@berkeley.edu>
2021-08-06 16:18:20 -0700

    list of API functions to implement

---
+++ src/pm_wrapper.jl
@@ -0,0 +1,9 @@
+run_opf(system::PSY.System, )
+run_pf(system::PSY.System, )
+compute_ac_pf(system::PSY.System, )
+run_ac_pf(system::PSY.System, )
+compute_dc_pf(system::PSY.System, )
+run_dc_pf(system::PSY.System, )
+
+run_model(system::PSY.System,)
+instantiate_model(system::PSY.System,)
