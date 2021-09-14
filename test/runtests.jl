ccaf18b504c502e353464c9e754148f68c3b0c28
Barrows <clayton.barrows@nrel.gov>
2021-09-13 23:01:44 -0600

    fixing error check in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -136,7 +136,7 @@ function run_tests()
             @includetests ARGS
         end
 
-        @test length(IS.get_log_events(multi_logger.tracker, Logging.Error)) == 0
+        @test length(IS.get_log_events(multi_logger.tracker, Logging.Error)) == 1
         @info IS.report_log_summary(multi_logger)
     end
 end
