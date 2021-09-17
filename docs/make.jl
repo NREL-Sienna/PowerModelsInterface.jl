5a6c55b660f7514f503023a4bcf819b191c10ec5
Barrows <clayton.barrows@nrel.gov>
2021-09-16 21:12:20 -0600

    formatting

--- docs/make.jl
+++ docs/make.jl
@@ -6,18 +6,14 @@ if haskey(ENV, "GITHUB_ACTIONS")
     ENV["JULIA_DEBUG"] = "Documenter"
 end
 
-pages = OrderedDict(
-        "Welcome Page" => "index.md",
-        "API" => "PowerModelsInterface.md"
-)
+pages = OrderedDict("Welcome Page" => "index.md", "API" => "PowerModelsInterface.md")
 
 # This code performs the automated addition of Literate - Generated Markdowns. The desired
 # section name should be the name of the file for instance network_matrices.jl -> Network Matrices
 # This code is generic to all SIIP documentation
 julia_file_filter = x -> occursin(".jl", x)
-folders = Dict(
-    "Developer Guide" => filter(julia_file_filter, readdir("docs/src/dev_guide")),
-)
+folders =
+    Dict("Developer Guide" => filter(julia_file_filter, readdir("docs/src/dev_guide")))
 
 for (section, folder) in folders
     for file in folder
@@ -25,21 +21,26 @@ for (section, folder) in folders
         outputdir = joinpath(pwd(), "docs", "src", "$section_folder_name")
         inputfile = joinpath("$section_folder_name", "$file")
         outputfile = string("generated_", replace("$file", ".jl" => ""))
-        Literate.markdown(joinpath(pwd(), "docs", "src", inputfile),
-                          outputdir;
-                          name = outputfile,
-                          credit = false,
-                          execute=true)
+        Literate.markdown(
+            joinpath(pwd(), "docs", "src", inputfile),
+            outputdir;
+            name = outputfile,
+            credit = false,
+            execute = true,
+        )
         subsection = titlecase(replace(split(file, ".")[1], "_" => " "))
-        push!(pages[section], ("$subsection" =>  joinpath("$section_folder_name", "$(outputfile).md")))
+        push!(
+            pages[section],
+            ("$subsection" => joinpath("$section_folder_name", "$(outputfile).md")),
+        )
     end
 end
 
 makedocs(
     modules = [InfrastructureSystems],
-    format = Documenter.HTML(prettyurls = haskey(ENV, "GITHUB_ACTIONS"),),
+    format = Documenter.HTML(prettyurls = haskey(ENV, "GITHUB_ACTIONS")),
     sitename = "PowerModelsInterface.jl",
-    pages = Any[p for p in pages]
+    pages = Any[p for p in pages],
 )
 
 deploydocs(
